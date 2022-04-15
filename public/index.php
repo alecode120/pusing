3ce9e2abe247dd7a38cb4c5c79ac3246a44677b0
ale <fakur.mustika3@gmail.com>
2022-04-16 00:03:01 +0700

    pertama commiet

---
+++ public/index.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
+
+
+use \RouterOS\Client;
+
+// Initiate client with config object
+$client = new Client([
+    'host'        => '101.255.151.118',
+    'user'        => 'abdul',
+    'pass'        => 'PPSI108oke',
+    'timeout'=>100,
+    'ssl'=>false,
+      'socket_options' => [
+        'bindto' => ':8999'
+    ]
+]);
+
+// Execute export command via ssh
+$response = $client->query('/export');
+// or
+$response = $client->export();
+
+var_dump($response);
